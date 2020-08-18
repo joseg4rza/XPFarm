386f95600b92ddbcc00283ed25d6a998c90c2276
Jose Luis Garza Gallegos <jose.garza.gallegos@ibm.com>
2020-08-17 19:06:26 -0500

    commit leer tiempo natural

--- berlinClock.js
+++ berlinClock.js
@@ -2,7 +2,13 @@
 //   Test.assertSimilar(berlinClock("12:56:01"), 
 //   "O\nRROO\nRROO\nYYRYYRYYRYY\nYOOO")
 exports.berlinClock = (time) => {
- var rSeconds=time[6]+time[7];
+  var rHours=time[0]+time[1]; //Receipt Hours from time
+  var rMinutes=time[3]+time[4]; //Receipt Minutes from time
+  var rSeconds=time[6]+time[7]; //Receipt Seconds from time
+  var panel='';
+ panel=rHours+rMinutes+rSeconds;
 
-  return rSeconds;
+  return panel;
+  
 }
+
--- tests/berlintest.test.js
+++ tests/berlintest.test.js
@@ -5,6 +5,11 @@ const {berlinClock} =require ('../berlinClock.js')
 // });
 
 // //Test 2 Que podemos recibir valor
+// it('should accept the correct 24h formatted input', () => {
+//     expect(berlinClock('23:52:35')).toBe('35');
+// });
+
+//Test 3 Que podemos recibir y leer hora natural en 3 variables
 it('should accept the correct 24h formatted input', () => {
-    expect(berlinClock('23:52:35')).toBe('35');
+    expect(berlinClock('23:52:35')).toBe('235235');
 });
