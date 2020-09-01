413e20a4eacd5973c1f750126d87f1d95b3b381e
Jose Luis Garza Gallegos <jose.garza.gallegos@ibm.com>
2020-08-31 23:41:51 -0500

    iT WORKS

--- berlinClock.js
+++ berlinClock.js
@@ -1,5 +1,5 @@
 function oneSecPanel(time) {
-  var receiptSeconds=time[7];
+  var receiptSeconds=time[6]+time[7];
   var oneSecondBulbPanel='O';
   if(receiptSeconds %2 === 0) {
     oneSecondBulbPanel='Y'
@@ -11,24 +11,17 @@ function oneSecPanel(time) {
 }
 function twentyHourPanel(time) {
   var receiptHours=time[0]+time[1]; 
-  var fiveHourBulbPanel='OOOO';
+  var twentyHourBulbPanel='';
   var fiveHourBulbs=Math.trunc(receiptHours/5)
-  if(fiveHourBulbs === 1) {
-    fiveHourBulbPanel='ROOO'+'\n';
+  var rFiller;
+  var oFiller;
+  for (rFiller = 0; rFiller< fiveHourBulbs; rFiller++) {
+    twentyHourBulbPanel=twentyHourBulbPanel+'R';
   }
-  else if(fiveHourBulbs === 2) {
-    fiveHourBulbPanel='RROO'+'\n';
+  for (oFiller = 0; oFiller < (4-fiveHourBulbs); oFiller++) {
+    twentyHourBulbPanel=twentyHourBulbPanel+'O';
   }
-  else if(fiveHourBulbs === 3) {
-    fiveHourBulbPanel='RRRO'+'\n';
-  }
-  else if(fiveHourBulbs === 4) {
-    fiveHourBulbPanel='RRRR'+'\n';
-  }
-  else if(fiveHourBulbs === 0) {
-    fiveHourBulbPanel='OOOO'+'\n';
-  }
-  return fiveHourBulbPanel;
+  return twentyHourBulbPanel+'\n';
 }
 function fourHourPanel(time) {
   var receiptHours=time[1]; 
@@ -89,22 +82,21 @@ function fiftyFiveMinPanel(time) {
 
 function fourMinPanel(time) {
   var receiptMinutes=time[4]; 
-  var oneMinBulbPanel='';
+  var fourMinBulbPanel='';
   var oneMinBulbs=receiptMinutes%5
   var yFiller;
   var oFiller;
   for (yFiller = 0; yFiller< oneMinBulbs; yFiller++) {
-    oneMinBulbPanel=oneMinBulbPanel+'Y';
+    fourMinBulbPanel=fourMinBulbPanel+'Y';
   }
   for (oFiller = 0; oFiller < (4-oneMinBulbs); oFiller++) {
-    oneMinBulbPanel=oneMinBulbPanel+'O';
+    fourMinBulbPanel=fourMinBulbPanel+'O';
   }
-  return oneMinBulbPanel+'\n';
+  return fourMinBulbPanel;
 }
 
 function berlinClock(Time) {
-   return twentyHourPanel(Time)+fourHourPanel(Time)+fiftyFiveMinPanel(Time)+fourMinPanel(Time);
-   //return fourHourPanel(Time);
+  return oneSecPanel(Time)+twentyHourPanel(Time)+fourHourPanel(Time)+fiftyFiveMinPanel(Time)+fourMinPanel(Time);
 }
 
 module.exports = {
--- tests/berlintest.test.js
+++ tests/berlintest.test.js
@@ -7,8 +7,13 @@ it('Identify the seconds, displaying the One-Second Panel on or off', () => {
     expect(oneSecPanel('02:12:30')).toBe("Y\n");
     expect(oneSecPanel('02:12:49')).toBe("O\n");
     expect(oneSecPanel('02:12:01')).toBe("O\n");
+    expect(oneSecPanel('02:12:04')).toBe("Y\n");
     expect(oneSecPanel('02:12:23')).toBe("O\n");
     expect(oneSecPanel('02:12:24')).toBe("Y\n");
+    expect(oneSecPanel('02:12:00')).toBe("Y\n");
+    expect(oneSecPanel('02:12:24')).toBe("Y\n");
+    expect(oneSecPanel('02:12:29')).toBe("O\n");
+    expect(oneSecPanel('02:12:10')).toBe("Y\n");
 });
 
 it('Identify the hours, displaying the twenty-hours panel', () => {
@@ -32,20 +37,22 @@ it('Identify the minutes, displaying the fiftyfive-minutes panel', () => {
 });
 
 it('Identify the minutes, displaying the four-minutes panel', () => {
-    expect(fourMinPanel('23:52:35')).toBe("YYOO\n");
-    expect(fourMinPanel('04:18:14')).toBe("YYYO\n");
-    expect(fourMinPanel('04:03:14')).toBe("YYYO\n");
-    expect(fourMinPanel('04:16:16')).toBe("YOOO\n");
-    expect(fourMinPanel('04:19:19')).toBe("YYYY\n");
-    expect(fourMinPanel('04:34:19')).toBe("YYYY\n");
-    expect(fourMinPanel('04:30:19')).toBe("OOOO\n");
+    expect(fourMinPanel('23:52:35')).toBe("YYOO");
+    expect(fourMinPanel('04:18:14')).toBe("YYYO");
+    expect(fourMinPanel('04:03:14')).toBe("YYYO");
+    expect(fourMinPanel('04:16:16')).toBe("YOOO");
+    expect(fourMinPanel('04:19:19')).toBe("YYYY");
+    expect(fourMinPanel('04:34:19')).toBe("YYYY");
+    expect(fourMinPanel('04:30:19')).toBe("OOOO");
 });
 
 it('should accept the correct 24h formatted input, and display complete clock', () => {
-    //expect(berlinClock('00:00:00')).toBe("Y\nOOOO\nOOOO\nOOOOOOOOOOO\nOOOO");
-    expect(berlinClock('22:46:29')).toBe("RRRR\nRROO\nYYRYYRYYROO\nYOOO");
-    //expect(berlinClock('23:52:35')).toBe("O\nRRRR\nRRRO\nYYRYYRYYRYO\nYYOO");
-    
-    //expect(berlinClock('00:00:00')).toBe("OOOO\n"); 
+    expect(berlinClock('00:00:00')).toBe("Y\nOOOO\nOOOO\nOOOOOOOOOOO\nOOOO");
+    expect(berlinClock('22:46:29')).toBe("O\nRRRR\nRROO\nYYRYYRYYROO\nYOOO");
+    expect(berlinClock('23:52:35')).toBe("O\nRRRR\nRRRO\nYYRYYRYYRYO\nYYOO");
+    expect(berlinClock('23:52:04')).toBe("Y\nRRRR\nRRRO\nYYRYYRYYRYO\nYYOO");
+    expect(berlinClock('23:52:36')).toBe("Y\nRRRR\nRRRO\nYYRYYRYYRYO\nYYOO");
+    expect(berlinClock('23:52:40')).toBe("Y\nRRRR\nRRRO\nYYRYYRYYRYO\nYYOO");
+    expect(berlinClock('23:52:00')).toBe("Y\nRRRR\nRRRO\nYYRYYRYYRYO\nYYOO");
 });
 
